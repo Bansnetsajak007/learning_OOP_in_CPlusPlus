05a38d02808d41483d4e09520b44710488d1eeab
kewadahal <110725776+ProfKdX7@users.noreply.github.com>
2023-08-28 18:02:13 +0545

    updated codes of string sortner

--- 6th.cpp
+++ 6th.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
-
+using namespace std;
 class StringSorter {
 public:
     StringSorter(int size) : size_(size) {
-        array_ = new std::string[size_];
+        array_ = new string[size_];
     }
 
     ~StringSorter() {
@@ -14,32 +14,32 @@ public:
     }
 
     void inputStrings() {
-        std::cout << "Enter " << size_ << " strings:" << std::endl;
+        cout << "Enter " << size_ << " strings:" << endl;
         for (int i = 0; i < size_; ++i) {
-            std::cin >> array_[i];
+            cin >> array_[i];
         }
     }
 
     void sortStrings() {
-        std::sort(array_, array_ + size_);
+        sort(array_, array_ + size_);
     }
 
     void displaySortedStrings() {
-        std::cout << "Sorted strings:" << std::endl;
+        cout << "Sorted strings:" << endl;
         for (int i = 0; i < size_; ++i) {
-            std::cout << array_[i] << std::endl;
+            cout << array_[i] << endl;
         }
     }
 
 private:
     int size_;
-    std::string* array_;
+    string* array_;
 };
 
 int main() {
     int size;
-    std::cout << "Enter the number of strings: ";
-    std::cin >> size;
+    cout << "Enter the number of strings: ";
+    cin >> size;
 
     StringSorter sorter(size);
     sorter.inputStrings();
